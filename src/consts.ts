204bd5f9576ad77844504bebc0d229b63d481b81
00tanveer <tanveer_fh09@yahoo.com>
2023-09-07 04:15:29 +0600

    first commit and nearly finished home page's 1st section, including header and footer

---
+++ src/consts.ts
@@ -0,0 +1,5 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = 'Wiser Magazine';
+export const SITE_DESCRIPTION = 'Welcome to a world of finance that you never knew could be so much fun and meaningful. Brought to you by WiserGates.';
