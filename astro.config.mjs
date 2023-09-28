b8ac4f883f436069eec30ba8501a8b530dc9c44b
00tanveer <tanveer_fh09@yahoo.com>
2023-09-28 15:35:01 +0600

    added sanity and make mazagine index working

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,13 +4,20 @@ import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import netlify from "@astrojs/netlify/functions";
 import sitemap from "@astrojs/sitemap";
-
 import react from "@astrojs/react";
+import sanity from "astro-sanity";
+
+import compress from "astro-compress";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://example.com",
-  integrations: [mdx(), sitemap(), react()],
-  output: "server",
-  adapter: netlify()
+  integrations: [mdx(), sitemap(), react(), sanity({
+    projectId: "k04j1s42",
+    dataset: "production",
+    apiVersion: "2023-02-08",
+    useCdn: false
+  }), compress()]
+  // output: "server",
+  // adapter: netlify(),
 });
---
+++ src/sanity/urlForImage.js
@@ -0,0 +1,12 @@
+/** @format */
+
+// /my-blog/frontend/src/sanity/urlForImage.js
+
+import { useSanityClient } from "astro-sanity";
+import imageUrlBuilder from "@sanity/image-url";
+
+export const imageBuilder = imageUrlBuilder(useSanityClient());
+
+export function urlForImage(source) {
+  return imageBuilder.image(source);
+}
