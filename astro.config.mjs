76b6dfae222de5aab3ea9cbfb3b849fef10d4612
00tanveer <tanveer_fh09@yahoo.com>
2024-08-27 10:56:17 +0600

    commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,13 +5,14 @@ import mdx from "@astrojs/mdx";
 import netlify from "@astrojs/netlify/functions";
 import sitemap from "@astrojs/sitemap";
 import react from "@astrojs/react";
+import preact from "@astrojs/preact";
 
 import compress from "astro-compress";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://example.com",
-  integrations: [mdx(), sitemap(), react(), compress()],
+  integrations: [mdx(), sitemap(), react(), compress(), preact()],
   output: "server",
   adapter: netlify(),
 });
